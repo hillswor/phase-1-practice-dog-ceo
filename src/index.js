8406d6623c74b46f6adf7acb15aed83da1581da7
Bruce <bruce.hillsworth@gmail.com>
2023-03-28 11:22:00 -0600

    Added two fetches.  One fetches dog images and appends them to DOM.  The other fetches dog breeds and appends them to DOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
-console.log('%c HI', 'color: firebrick')
+document.addEventListener("DOMContentLoaded", () => {
+	const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+	const breedUrl = "https://dog.ceo/api/breeds/list/all";
+	const dogImageContainer = document.querySelector("#dog-image-container");
+	const dogBreedList = document.querySelector("#dog-breeds");
+
+	fetch(imgUrl) //Loads imgages on page
+		.then((response) => response.json())
+		.then((dogObject) => {
+			dogObject.message.forEach((dogImage) => {
+				const img = document.createElement("img");
+				img.setAttribute("src", `${dogImage}`);
+				dogImageContainer.appendChild(img);
+			});
+		});
+
+	fetch(breedUrl)
+		.then((response) => response.json())
+		.then((dogsObject) => {
+			const dogBreedArray = Object.keys(dogsObject.message);
+			dogBreedArray.forEach((breed) => {
+				const li = document.createElement("li");
+				li.textContent = breed;
+				dogBreedList.appendChild(li);
+			});
+		});
+});
